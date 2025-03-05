4c99a6a42090929b875187d56ba6282a6d147302
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-05 14:31:18 +0600

    spred Operator max  commit

--- module-30.1/spredOperator.js
+++ module-30.1/spredOperator.js
@@ -5,3 +5,6 @@ const spresdOprtr = ["tanjil", "tanvir", 'rana'];
 
 const concat = ['marfot', ...spresdOprtr];
 console.log(concat);
+
+const math =[12,21,2,1,3,13,32,3,42,323,21];
+console.log(Math.max(...math));
