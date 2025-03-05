caf8a668da0953c8f9e028d1b4f37094fcc5125c
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-05 14:46:59 +0600

    object destructuring  commit

---
+++ module-30.1/arrayObjectDistructuring.js
@@ -0,0 +1,13 @@
+let distructur = {
+    name: 'tanjil',
+    age: 21,
+    hoby : 'eating ',
+};
+
+let disName =distructur.name;
+let disage =distructur.age;
+let dishoby =distructur.hoby;
+console.log(dishoby);
+
+let {name, age, hoby}= distructur;
+console.log(age);
