35dc7d2807b558922b76a7afc974354cbfe634cf
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-05 15:22:08 +0600

    object freeze  commit

--- module-30.1/keys-velue-enrtries-deket-deal-freeze.js
+++ module-30.1/keys-velue-enrtries-deket-deal-freeze.js
@@ -2,9 +2,13 @@ let obkvdf= {
     name: 'tanjil',
     age: 21,
 }
-Object.seal(obkvdf);
+// Object.seal(obkvdf);
+Object.freeze(obkvdf);
 
-obkvdf.mobile= 'not found'
+obkvdf.age= 22;
+obkvdf.mobile= 'not found';
+
+
 console.log(Object.keys(obkvdf));
 console.log(Object.values(obkvdf));
 console.log(Object.entries(obkvdf));
