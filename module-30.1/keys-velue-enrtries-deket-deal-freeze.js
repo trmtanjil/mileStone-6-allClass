837d3a633bf615897e1276e13d26c5c48d234493
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-05 15:18:01 +0600

    object seal  commit

--- module-30.1/keys-velue-enrtries-deket-deal-freeze.js
+++ module-30.1/keys-velue-enrtries-deket-deal-freeze.js
@@ -2,6 +2,10 @@ let obkvdf= {
     name: 'tanjil',
     age: 21,
 }
+Object.seal(obkvdf);
+
+obkvdf.mobile= 'not found'
 console.log(Object.keys(obkvdf));
 console.log(Object.values(obkvdf));
 console.log(Object.entries(obkvdf));
+console.log(obkvdf);
