1eea19a67fc61be26e55e1e7ae1b96577175eca9
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-07 10:02:11 +0600

    truthy value falsy value  commit

---
+++ module-32/truthyFalsyValue.js
@@ -0,0 +1,10 @@
+let name = 'tanjil';
+let namee =0n;
+
+if(namee){
+console.log('this value is truthy value');
+}
+else{
+    console.log('this value is falsy value');
+}
+if(name)console.log(true);
