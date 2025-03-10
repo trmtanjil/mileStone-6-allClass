d8d898f78938307c26fddebc4e5e10858403e11c
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-10 23:16:11 +0600

    Load debut api  commit

--- module-33/post.js
+++ module-33/post.js
@@ -10,7 +10,6 @@ const handlpost=()=>{
 const subtractFung = (getPosts)=>{
     const postDisplayy=   document.getElementById('postDisplay')
     for(let getPost of getPosts){
-     console.log(getPost.body);
     const div = document.createElement('div');
     div.classList.add('card');
      div.innerHTML=`
@@ -22,3 +21,20 @@ const subtractFung = (getPosts)=>{
     }
 }
 handlpost()
+
+const putbutton=()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts/1', {
+        method: 'PUT',
+        body: JSON.stringify({
+          id: 1,
+          title: 'foo',
+          body: 'bar',
+          userId: 1,
+        }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+}
