513d8e4bfc4d5fec654059def7dce08e437bc592
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-10 19:14:13 +0600

    json stringify cmmit

---
+++ module-32/equal.js
@@ -0,0 +1,17 @@
+let name = 10;
+let name1 = '10';
+
+if(name==name1){
+    console.log('this is eaual value');
+}
+else{
+    console.log('this is Not eaual value');
+
+}
+if(5+'2'===7){
+    console.log(true);
+}
+else{
+    console.log(false);;
+}
+console.log(5+'2'===7);
---
+++ module-33/json.js
@@ -0,0 +1,14 @@
+const person = {
+    Name : "tanjil",
+    age : 10,
+    friends: ['sharif', "hosain", 'aziz']
+}
+console.log(person);
+
+const newperson = JSON.stringify(person);
+console.log(newperson);
+
+
+const  jsonclick =()=>{
+    console.log('click')
+}
