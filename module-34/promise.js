9fd1a4c059d8b02a0076805e0c6363ec9871f11d
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-11 11:31:04 +0600

    prommise map  commit

--- module-34/promise.js
+++ module-34/promise.js
@@ -19,24 +19,39 @@
 
                 //? another way 
 
-const fetchData=()=>{
-  return  new Promise((resolve, reject) => {
-        const status = false;
-        if(status){
-          const mocdata={
-            json:()=>Promise.resolve({name:'tanjil'})
-          }
-          resolve(mocdata)
-        }
-        else{
-            reject('server error')
-        }
-    })
-}
+// const fetchData=()=>{
+//   return  new Promise((resolve, reject) => {
+//         const status = false;
+//         if(status){
+//           const mocdata={
+//             json:()=>Promise.resolve({name:'tanjil'})
+//           }
+//           resolve(mocdata)
+//         }
+//         else{
+//             reject('server error')
+//         }
+//     })
+// }
 
-fetchData()
+// fetchData()
+// .then(res=>res.json())
+// .then(data=>console.log(data))
 
-.then(res=>res.json())
+// .catch(err=>console.log(err))
+
+const urls=[
+    'https://jsonplaceholder.typicode.com/posts/1',
+    'https://jsonplaceholder.typicode.com/posts/1/comments',
+    'https://jsonplaceholder.typicode.com/posts/1',
+    'https://jsonplaceholder.typicode.com/comments?postId=1'
+];
+
+Promise.all(urls.map(url=>{
+    return fetch(url)
+    .then(res=>res.json())
+    .then(data=>data)
+}))
+.then(res => res)
 .then(data=>console.log(data))
-
-.catch(err=>console.log(err))
+.catch(error=>console.log('got error',error))
