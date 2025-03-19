532b7aa71fc0b0cf0678cc876263bf04155eedf2
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-19 09:13:34 +0600

    local perse string storage

--- module-36.5/index.js
+++ module-36.5/index.js
@@ -1,5 +1,28 @@
-const name = 'tanjil';
+// const name = 'tanjil';
+// localStorage.setItem('my name is',  name)
+// console.log(localStorage.getItem('my name is'));
 
-localStorage.setItem('my name is',  name)
 
-console.log(localStorage.getItem('my name is'));
+
+// const product = document.getElementById('textfields');
+// const productValue = product.value;
+
+// const quantity = document.getElementById('quantityFields');
+// const quantityValue = quantity.value;
+
+
+
+
+function clickSetItem(){
+const person={
+    name:'tanjil',
+    age: 23,
+    stydy: true,
+}
+const convertoStringyfy = JSON.stringify(person);
+localStorage.setItem('my info',convertoStringyfy)
+
+const convertPers= JSON.parse(convertoStringyfy)
+console.log(convertPers.name);
+}
+clickSetItem()
