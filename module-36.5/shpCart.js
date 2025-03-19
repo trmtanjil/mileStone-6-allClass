e78c9c0c70dc979f1933c426847b929021796862
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-03-20 00:27:36 +0600

    Local Storage Interactions for a shopping Cart & practice task commit

--- module-36.5/shpCart.js
+++ module-36.5/shpCart.js
@@ -4,8 +4,8 @@ addtocart=()=>{
     const name = inputProduct.value;
     const quantity = inputQuantity.value;
     productQuantity(name, quantity);
-    settoCardContainer(name,quantity)
-    containers(name, quantity)
+    saveProductLocalStorage(name,quantity)
+    // containers(name, quantity)
 
     inputProduct.value='';
     inputQuantity.value='';
@@ -19,16 +19,33 @@ const productQuantity =(pruducts, quantitys)=>{
     `;
     container.appendChild(li)
 }
-const settoCardContainer=(pruducts,quantitys)=>{
-     const newProducts = {
-        name: 'tanjil',
-     }
-     const convert=JSON.stringify(newProducts)
-     localStorage.setItem('pruducts',convert)
 
-
-    console.log(pruducts,quantitys);
+const getPoductlocalStorage=()=>{
+  let cart ={};
+  const getProduct= localStorage.getItem("cart");
+  if(getProduct){
+    cart = JSON.parse(getProduct)
+  } 
+    return cart;
 }
 
 
+const displayProductLocalStorag=()=>{
+  const products = getPoductlocalStorage();
+   for(const productt in products){
+    productQuantity(productt,products[productt])
+   }
+}
+
+
+const saveProductLocalStorage=(pruducts,quantitys)=>{
+  const cart= getPoductlocalStorage();
+
+  cart[pruducts]= quantitys;
+   const converString = JSON.stringify(cart);
+   localStorage.setItem("cart",converString)
+   console.log(converString);
+}
+
+displayProductLocalStorag()
  
